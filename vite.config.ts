da726ee15d01c08aa2c48a73c42acf2f1329a328
koujianfeng <koujianfeng@do-global.com>
2026-03-18 11:59:28 +0800

    feat: 新增 GitHub Pages 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/ko-interview/' : '/',
   plugins: [
     vue(),
     AutoImport({
